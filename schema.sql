f0ffefc7c413648cafddc2dc4144ccdf64515783
Chayce Merchant <chaycepmerchant@gmail.com>
2026-04-16 01:57:36 -0500

    Fixed errors in the schema.sql file

--- schema.sql
+++ schema.sql
@@ -13,13 +13,13 @@ CREATE TABLE Employee (
 
 CREATE TABLE Vehicle (
     vehicle_id INT AUTO_INCREMENT PRIMARY KEY,
-    vin INT UNIQUE NOT NULL,
+    vin VARCHAR(17) UNIQUE NOT NULL,
     make VARCHAR(50),
     MODEL VARCHAR(50),
     year INT,
     price DECIMAL(10, 2),
     status ENUM('available', 'sold', 'maintenance') DEFAULT 'available',
-    location ENUM('main_lot', 'remote_lot', 'other_dealership', 'in_trasnsit')
+    location ENUM('main_lot', 'remote_lot', 'other_dealership', 'in_transit')
 );
 
 CREATE TABLE Customer (
@@ -28,7 +28,7 @@ CREATE TABLE Customer (
     address VARCHAR(255),
     phone VARCHAR(20),
     email VARCHAR(255),
-    customer_type ENUM('purhcase', 'service', 'visit')
+    customer_type ENUM('purchase', 'service', 'visit')
 );
 
 CREATE TABLE Billing (
@@ -36,6 +36,7 @@ CREATE TABLE Billing (
     billing_date DATE,
     payment_method ENUM('cash', 'credit_card', 'debit_card', 'check'),
     goods_or_services VARCHAR(50),
+    amount DECIMAL(10, 2) NOT NULL,
     customer_id INT,
     billing_staff_id INT,
     FOREIGN KEY (customer_id) REFERENCES Customer(customer_id),
@@ -47,16 +48,18 @@ CREATE TABLE Sale (
     sale_date DATE,
     temp_tag_num VARCHAR(20),
     customization_work VARCHAR(255),
+    billing_id INT,
     customer_id INT,
     vehicle_id INT,
     salesperson_id INT,
+    FOREIGN KEY (billing_id) REFERENCES Billing(billing_id),
     FOREIGN KEY (customer_id) REFERENCES Customer(customer_id),
-    FOREIGN KEY (vehicle_id) REFERNCES Vehicle(vehicle_id),
-    FOREIGN KEY (salesperson_id) REFERNCES Employee(employee_id)
+    FOREIGN KEY (vehicle_id) REFERENCES Vehicle(vehicle_id),
+    FOREIGN KEY (salesperson_id) REFERENCES Employee(employee_id)
 );
 
 CREATE TABLE Service (
-    serivice_id INT AUTO_INCREMENT PRIMARY KEY,
+    service_id INT AUTO_INCREMENT PRIMARY KEY,
     arrival_mileage INT,
     departure_mileage INT,
     preliminary_estimate DECIMAL(10, 2),
